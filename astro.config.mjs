b150dc5ab4ff38a250ed406e0630497578b3f2af
abengkris <abengkrischannel@gmail.com>
2024-04-25 22:11:07 +0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,50 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [
-		starlight({
-			// site: 'https://cuddly-space-pancake-wqwg6996ww52v9gx-4321.app.github.dev',
-			title: 'IndoNostr.xyz',
-			locales: {
-				root: {
-				  label: 'Indonesian',
-				  lang: 'id',
-				},
-			  },
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Panduan',
-					items: [
-						{ label: 'Apa itu Nostr?', link: '/apa-itu-nostr' },
-						{ label: 'Mulai menggunakan Nostr', link: '/panduan/mulai-menggunakan-nostr' },
-					],
-				},
-				{
-					label: 'Protokol',
-					items: [
-						{ label: 'Nostr tingkat atas', link: '/protokol/nostr-tingkat-atas' },
-						{ label: 'Event', link: '/protokol/events' },
-						{ label: 'NIP', link: '/protokol/nostr-implementation-possibilities-nip' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  output: "server",
+  integrations: [starlight({
+    // site: 'https://cuddly-space-pancake-wqwg6996ww52v9gx-4321.app.github.dev',
+    title: 'IndoNostr.xyz',
+    locales: {
+      root: {
+        label: 'Indonesian',
+        lang: 'id'
+      }
+    },
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Panduan',
+      items: [{
+        label: 'Apa itu Nostr?',
+        link: '/apa-itu-nostr'
+      }, {
+        label: 'Mulai menggunakan Nostr',
+        link: '/panduan/mulai-menggunakan-nostr'
+      }]
+    }, {
+      label: 'Protokol',
+      items: [{
+        label: 'Nostr tingkat atas',
+        link: '/protokol/nostr-tingkat-atas'
+      }, {
+        label: 'Event',
+        link: '/protokol/events'
+      }, {
+        label: 'NIP',
+        link: '/protokol/nostr-implementation-possibilities-nip'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })],
+  adapter: vercel()
 });

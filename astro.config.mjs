7e0968ee1e7f77a8e477ca40b1ace4d7955ddd2c
abengkris <abengkrischannel@gmail.com>
2024-04-26 16:18:41 +0700

    menambah sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,74 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [starlight({
-    // site: 'https://cuddly-space-pancake-wqwg6996ww52v9gx-4321.app.github.dev',
-    title: 'IndoNostr.xyz',
-    locales: {
-      root: {
-        label: 'Indonesian',
-        lang: 'id'
-      }
-    },
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'Panduan',
-      items: [{
-        label: 'Apa itu Nostr?',
-        link: '/apa-itu-nostr'
-      }, {
-        label: 'Mulai menggunakan Nostr',
-        link: '/panduan/mulai-menggunakan-nostr'
-      }]
-    }, {
-      label: 'Protokol',
-      items: [{
-        label: 'Nostr tingkat atas',
-        link: '/protokol/nostr-tingkat-atas'
-      }, {
-        label: 'Event',
-        link: '/protokol/events'
-      }, {
-        label: 'NIP',
-        link: '/protokol/nostr-implementation-possibilities-nip'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  })],
+  integrations: [
+    starlight({
+      // site: "indonostr.xyz",
+      title: "IndoNostr.xyz",
+      locales: {
+        root: {
+          label: "Indonesian",
+          lang: "id",
+        },
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Panduan",
+          items: [
+            {
+              label: "Apa itu Nostr?",
+              link: "/apa-itu-nostr",
+            },
+            {
+              label: "Mulai menggunakan Nostr",
+              link: "/panduan/mulai-menggunakan-nostr",
+            },
+          ],
+        },
+        {
+          label: "Protokol",
+          items: [
+            {
+              label: "Nostr tingkat atas",
+              link: "/protokol/nostr-tingkat-atas",
+            },
+            {
+              label: "Event",
+              link: "/protokol/events",
+            },
+            {
+              label: "NIP",
+              link: "/protokol/nostr-implementation-possibilities-nip",
+            },
+          ],
+        },
+        {
+          label: "Klien dan relay",
+          items: [
+            {
+              label: "Klien",
+              link: "/klien",
+            },
+            {
+              label: "Relay",
+              link: "/relay",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
-  })
+  }),
 });
